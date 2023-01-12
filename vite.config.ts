d117a40e9b49901adeb56ce6abc089a782d1af35
heiwa4126 <heiwa4126@gmail.com>
2023-01-12 15:29:41 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { ConfigEnv, defineConfig, UserConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ command }: ConfigEnv) => {
+  const cfg: UserConfig = {
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            v: ["react", "react-dom", "react-router-dom"],
+          },
+        },
+      },
+    },
+  };
+
+  if (command === "build") {
+    // when `vite build`
+    cfg.esbuild = {
+      drop: ["console", "debugger"], // https://esbuild.github.io/api/#drop
+    };
+  }
+
+  return cfg;
+});
